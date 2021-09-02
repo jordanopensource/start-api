ed06e57732166c54fe602dd89979f38250290664
wissam <wissam_ataleh@outlook.com>
2021-09-02 13:55:32 +0300

    Add url server setting

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('BASE_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8befe19536e68ad005903db1bb66acdc'),
